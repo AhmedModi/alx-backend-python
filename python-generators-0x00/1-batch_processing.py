56948fce18d65b01881ecc68bbb00530f290f091
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-05 10:41:49 +0100

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 import json
 
+
 def stream_users_in_batches(batch_size):
-    """Generator that yields users in batches from the database file."""
+    """
+    Generator that yields user data in batches of size `batch_size`.
+    """
     with open("user_data.json", "r") as f:
         users = json.load(f)
 
-    total_users = len(users)
-    for i in range(0, total_users, batch_size):
-        yield users[i:i + batch_size]
+    index = 0
+    while index < len(users):
+        yield users[index: index + batch_size]
+        index += batch_size
+
 
 def batch_processing(batch_size):
-    """Processes each batch to print users over the age of 25."""
+    """
+    Processes batches of users, filtering and printing users older than 25.
+    """
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
-            if user.get('age', 0) > 25:
+            if user.get("age", 0) > 25:
                 print(user)
