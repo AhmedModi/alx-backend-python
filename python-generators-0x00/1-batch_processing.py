2611426f42e1755e0f1af8c5242602df5f1bb972
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-05 10:45:42 +0100

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -2,38 +2,30 @@ import mysql.connector
 from mysql.connector import Error
 
 def stream_users_in_batches(batch_size=1000):
-    """Generator that yields batches of users from the database"""
-    connection = None
-    cursor = None
-    
+    """Generator that yields batches of users from database"""
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='ALX_prodev'
+    )
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
     try:
-        connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='',
-            database='ALX_prodev'
-        )
-        
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM user_data")
-        
         while True:
             batch = cursor.fetchmany(batch_size)
             if not batch:
                 break
             yield batch
-            
-    except Error as e:
-        print(f"Database error: {e}")
     finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
+        cursor.close()
+        connection.close()
 
 def batch_processing(batch_size=1000):
     """Process batches of users, filtering those over 25 years old"""
-    for batch in stream_users_in_batches(batch_size):
+    batch_gen = stream_users_in_batches(batch_size)
+    for batch in batch_gen:
         for user in batch:
             if user['age'] > 25:
                 yield user
