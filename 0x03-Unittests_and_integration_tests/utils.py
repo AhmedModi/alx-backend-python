3a5c25ff385a4d261ff7766485013d098a1b901c
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-19 14:35:52 +0100

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -2,7 +2,9 @@
 """Utils module"""
 
 def access_nested_map(nested_map, path):
-    """Access a nested map with a given path"""
     for key in path:
-        nested_map = nested_map[key]
+        try:
+            nested_map = nested_map[key]
+        except (KeyError, TypeError):
+            raise KeyError(key)
     return nested_map
