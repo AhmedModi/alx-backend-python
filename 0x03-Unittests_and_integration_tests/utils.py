432c2247c3f74b5c378f11b5bfd0914bf8b56fbb
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-19 13:53:01 +0100

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""utils module with access_nested_map function"""
+"""utils module with utility functions for accessing nested maps and memoization"""
 
 
 def access_nested_map(nested_map, path):
@@ -7,3 +7,23 @@ def access_nested_map(nested_map, path):
     for key in path:
         nested_map = nested_map[key]
     return nested_map
+
+
+def get_json(url):
+    """Get JSON from a given URL"""
+    import requests
+    res = requests.get(url)
+    return res.json()
+
+
+class Memoize:
+    """Simple memoization class"""
+
+    def __init__(self, func):
+        self.func = func
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+            self.memo[args] = self.func(*args)
+        return self.memo[args]
