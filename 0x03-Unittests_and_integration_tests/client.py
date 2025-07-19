ff9fe2b7f42272218508a087c63fbd2244da9dba
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-19 13:56:16 +0100

    Update client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,30 +1,48 @@
 #!/usr/bin/env python3
-"""Client module containing GithubOrgClient"""
+"""GithubOrgClient class"""
 
 import requests
+from typing import List, Dict
+from utils import get_json
 
 
 class GithubOrgClient:
-    """GithubOrgClient class to interact with GitHub API"""
+    """Client for GitHub organization info"""
 
     ORG_URL = "https://api.github.com/orgs/{org}"
 
-    def __init__(self, org_name):
+    def __init__(self, org_name: str):
+        """Initialize client with organization name"""
         self.org_name = org_name
 
-    def org(self):
-        """Return organization data from GitHub"""
-        url = self.ORG_URL.format(org=self.org_name)
-        return requests.get(url).json()
+    @property
+    def org(self) -> Dict:
+        """Fetch organization info"""
+        return get_json(self.ORG_URL.format(org=self.org_name))
 
     @property
-    def _public_repos_url(self):
-        """Extracts the repos_url from the organization payload"""
-        return self.org().get("repos_url")
+    def _public_repos_url(self) -> str:
+        """Return the URL for the public repos"""
+        return self.org.get("repos_url", "")
 
-    def public_repos(self, license=None):
-        """Returns the list of public repositories for the org"""
-        repos = requests.get(self._public_repos_url).json()
-        if license is None:
-            return [repo["name"] for repo in repos]
-        return [repo["name"] for repo in repos if repo.get("license", {}).get("key") == license]
+    def public_repos(self, license: str = None) -> List[str]:
+        """Return names of all public repositories"""
+        repos = get_json(self._public_repos_url)
+        repo_names = [repo["name"] for repo in repos]
+
+        if license:
+            repo_names = [
+                repo["name"]
+                for repo in repos
+                if self.has_license(repo, license)
+            ]
+
+        return repo_names
+
+    @staticmethod
+    def has_license(repo: Dict, license_key: str) -> bool:
+        """Check if the repo has a specific license"""
+        try:
+            return repo.get("license", {}).get("key") == license_key
+        except AttributeError:
+            return False
