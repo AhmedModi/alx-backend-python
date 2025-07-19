18163bbdddbde243ece306c3e3b7d1242fa7feb8
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-19 14:00:31 +0100

    Update client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python3
-"""GithubOrgClient module"""
+"""Github Org Client"""
 
 import requests
-
-
-def get_json(url):
-    """GET JSON from a URL"""
-    response = requests.get(url)
-    return response.json()
+from typing import Dict, List
+from functools import lru_cache
 
 
 class GithubOrgClient:
-    """Github Organization Client"""
+    """Github client for org info"""
 
-    ORG_URL = "https://api.github.com/orgs/{}"
+    ORG_URL = "https://api.github.com/orgs/{org}"
 
-    def __init__(self, org_name):
+    def __init__(self, org_name: str) -> None:
         self.org_name = org_name
 
     @property
-    def org(self):
-        """Fetch the organization info"""
-        return get_json(self.ORG_URL.format(self.org_name))
+    @lru_cache()
+    def org(self) -> Dict:
+        """Get org data from GitHub"""
+        return requests.get(self.ORG_URL.format(org=self.org_name)).json()
 
-    def has_license(self, repo, license_key):
-        """Check if the repo has a specific license"""
+    @property
+    def _public_repos_url(self) -> str:
+        """Extracts repos_url from org"""
+        return self.org.get("repos_url", "")
+
+    def public_repos(self) -> List[str]:
+        """Fetches public repos"""
+        repos = requests.get(self._public_repos_url).json()
+        return [repo["name"] for repo in repos]
+
+    def has_license(self, repo: Dict, license_key: str) -> bool:
+        """Checks if repo has license"""
         try:
-            return repo.get("license", {}).get("key") == license_key
-        except AttributeError:
+            return repo["license"]["key"] == license_key
+        except Exception:
             return False
