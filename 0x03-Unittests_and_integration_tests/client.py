0ed354bd8637c642013db8dcb538d7dd76930f4f
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-19 13:57:48 +0100

    Update client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,46 +1,29 @@
 #!/usr/bin/env python3
-"""GithubOrgClient class"""
+"""GithubOrgClient module"""
 
 import requests
-from typing import List, Dict
-from utils import get_json
+
+
+def get_json(url):
+    """GET JSON from a URL"""
+    response = requests.get(url)
+    return response.json()
 
 
 class GithubOrgClient:
-    """Client for GitHub organization info"""
+    """Github Organization Client"""
 
-    ORG_URL = "https://api.github.com/orgs/{org}"
+    ORG_URL = "https://api.github.com/orgs/{}"
 
-    def __init__(self, org_name: str):
-        """Initialize client with organization name"""
+    def __init__(self, org_name):
         self.org_name = org_name
 
     @property
-    def org(self) -> Dict:
-        """Fetch organization info"""
-        return get_json(self.ORG_URL.format(org=self.org_name))
+    def org(self):
+        """Fetch the organization info"""
+        return get_json(self.ORG_URL.format(self.org_name))
 
-    @property
-    def _public_repos_url(self) -> str:
-        """Return the URL for the public repos"""
-        return self.org.get("repos_url", "")
-
-    def public_repos(self, license: str = None) -> List[str]:
-        """Return names of all public repositories"""
-        repos = get_json(self._public_repos_url)
-        repo_names = [repo["name"] for repo in repos]
-
-        if license:
-            repo_names = [
-                repo["name"]
-                for repo in repos
-                if self.has_license(repo, license)
-            ]
-
-        return repo_names
-
-    @staticmethod
-    def has_license(repo: Dict, license_key: str) -> bool:
+    def has_license(self, repo, license_key):
         """Check if the repo has a specific license"""
         try:
             return repo.get("license", {}).get("key") == license_key
