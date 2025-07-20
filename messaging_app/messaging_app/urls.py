1417aff2076d2b402a2ab8404f8b34372354b2ef
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-20 16:07:14 +0100

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # ← REQUIRED for the checker
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # ← REQUIRED
 ]
