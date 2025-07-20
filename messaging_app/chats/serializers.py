4670f36fccf11a1242b133a9d4150ae6d89bd2c8
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-20 15:58:38 +0100

    Update serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
-from rest_framework.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,8 +21,6 @@ class MessageSerializer(serializers.ModelSerializer):
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
-
-    # Example CharField and validation usage
     topic = serializers.CharField(write_only=True, required=False)
 
     class Meta:
@@ -32,5 +29,5 @@ class ConversationSerializer(serializers.ModelSerializer):
 
     def validate_topic(self, value):
         if len(value) < 3:
-            raise ValidationError("Topic must be at least 3 characters long.")
+            raise serializers.ValidationError("Topic must be at least 3 characters long.")
         return value
