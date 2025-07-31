ef8d28462f20a260d6ae4ec8f977f1a43764a220
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-31 15:57:46 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,12 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Message
-from django.contrib.auth.decorators import login_required
 
 @login_required
-def threaded_messages_view(request):
-    # Fetch parent messages (non-replies)
-    messages = Message.objects.filter(receiver=request.user, parent_message__isnull=True)\
-        .select_related('sender', 'receiver')\
-        .prefetch_related('replies__sender', 'replies__receiver')
+def sent_messages_view(request):
+    messages = Message.objects.filter(sender=request.user)\
+        .select_related('receiver')\
+        .prefetch_related('replies')
 
-    return render(request, 'messaging/threaded_messages.html', {'messages': messages})
+    return render(request, 'messaging/sent_messages.html', {'messages': messages})
