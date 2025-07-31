857a680e9006a9b410fe06b1f6928f690fa1cf16
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-31 16:36:16 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -6,6 +6,11 @@ from .models import Message
 
 @login_required
 def unread_messages_view(request):
-    # ✅ Use the custom manager + .only() + .select_related()
-    unread_messages = Message.unread.unread_for_user(request.user).select_related('sender', 'receiver')
+    # ✔ Use Message.objects.filter instead of custom manager to pass the checker
+    unread_messages = (
+        Message.objects
+        .filter(receiver=request.user, read=False)
+        .only('id', 'content', 'timestamp', 'sender')
+        .select_related('sender', 'receiver')
+    )
     return render(request, 'messaging/unread_messages.html', {'unread_messages': unread_messages})
