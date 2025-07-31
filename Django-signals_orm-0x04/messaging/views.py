dccc2fbe2aed1aca80d5d164cc08dac4e632017f
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-31 15:54:33 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .models import Message
 
 @login_required
 def delete_user(request):
@@ -9,3 +11,12 @@ def delete_user(request):
     logout(request)  # Log user out before deleting
     user.delete()
     return redirect('home')  # Replace with your actual redirect URL
+
+@login_required
+def threaded_messages_view(request):
+    # Fetch parent messages (non-replies)
+    messages = Message.objects.filter(receiver=request.user, parent_message__isnull=True)\
+        .select_related('sender', 'receiver')\
+        .prefetch_related('replies__sender', 'replies__receiver')
+
+    return render(request, 'messaging/threaded_messages.html', {'messages': messages})
