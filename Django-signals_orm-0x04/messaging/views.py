9ea4c7d4df1a298df8228d968d4ed8aba7d56319
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-31 19:41:51 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -3,6 +3,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Message
+from django.views.decorators.cache import cache_page
+from messaging.models import Message
+
+@cache_page(60)  # Caches the view for 60 seconds
+def conversation_detail(request, conversation_id):
+    messages = Message.objects.filter(conversation_id=conversation_id)
+    return render(request, 'chats/conversation_detail.html', {'messages': messages})
 
 @login_required
 def unread_messages_view(request):
