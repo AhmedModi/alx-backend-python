3e3c0ed745caf261d4ec489cf1342e2b46d12748
Ahmed Modee <46077468+AhmedModi@users.noreply.github.com>
2025-07-31 16:34:39 +0100

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,11 @@
-from django.contrib.auth.decorators import login_required
+# messaging/views.py
+
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Message
 
 @login_required
 def unread_messages_view(request):
-    unread_messages = Message.unread.for_user(request.user)
+    # ✅ Use the custom manager + .only() + .select_related()
+    unread_messages = Message.unread.unread_for_user(request.user).select_related('sender', 'receiver')
     return render(request, 'messaging/unread_messages.html', {'unread_messages': unread_messages})
